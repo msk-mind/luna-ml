8ebc1f690e4c4d2bbe08ab74f1ab92327e69e408
aauker <ataukerman@mail.com>
2021-11-04 10:32:20 -0400

    Initial test model

---
+++ hubconf.py
@@ -0,0 +1,5 @@
+# Optional list of dependencies required by the package
+dependencies = ["torch"]
+
+# classification
+from models.logistic_regression import logistic_regression
---
+++ models/logistic_regression.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self) -> None:
+        super(LogisticRegression, self).__init__()
+        self.lin1 = nn.Linear(100, 1)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, input):
+        return self.sigmoid(self.lin1(input))
+
+def logistic_regression():
+    model = LogisticRegression()
+    state_dict = torch.load("weights/logistic_regression_random.pth")
+    model.load_state_dict(state_dict)
+    return model
