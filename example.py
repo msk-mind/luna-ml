9340787c699b68f08a76ea949358c9cbc795ead8
aauker <ataukerman@mail.com>
2021-11-04 11:27:50 -0400

    Fix path

--- example.py
+++ example.py
@@ -7,3 +7,6 @@ print (model.lin1.weight)
 
 model = torch.hub.load("msk-mind/luna-ml:main",  "logistic_regression", weight_tag="main:logistic_regression_random.pth")
 print (model.lin1.weight)
+
+model = torch.hub.load("msk-mind/luna-ml:main", "tissue_tile_net", weight_tag="main:tissue_net_2021-01-19_21.05.24-e17.pth")
+print (model)
