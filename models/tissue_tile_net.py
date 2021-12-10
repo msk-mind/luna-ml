79a58602b6cb6e5052b9daecfe3c7416bbc963ad
Andy Aukerman <ataukerman@gmail.com>
2021-12-10 16:26:35 -0500

    Update tissue_tile_net.py

--- models/tissue_tile_net.py
+++ models/tissue_tile_net.py
@@ -5,7 +5,7 @@ from torchvision.models import resnet18, resnet34, resnet50, squeezenet1_1, vgg1
 from utils import get_state_dict_from_git_tag
 
 class TissueTileNet(torch.nn.Module):
-    def __init__(self, model, num_classes, activation=None):
+    def __init__(self, model, num_classes):
         super(TissueTileNet, self).__init__()
         if type(model) in [torchvision.models.resnet.ResNet]:
             model.fc = torch.nn.Linear(512, num_classes)
@@ -14,7 +14,7 @@ class TissueTileNet(torch.nn.Module):
         else:
             raise NotImplementedError
         self.model = model
-        self.activation = activation
+        self.activation = torch.nn.Softmax(dim=1)
 
     def forward(self, x):
         y = self.model(x)
@@ -31,7 +31,7 @@ def tissue_tile_net_transform ():
         torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ])
 
-def tissue_tile_net_model (activation, num_classes, weight_tag=None):
+def tissue_tile_net_model (num_classes, weight_tag=None):
     model = TissueTileNet(resnet18(), num_classes, activation=activation)
     if weight_tag:
         state_dict = get_state_dict_from_git_tag(weight_tag)
