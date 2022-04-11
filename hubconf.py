fdeb250d062f52fae6d26118f7cecc893e1d9a3a
Andy Aukerman <ataukerman@gmail.com>
2022-04-11 10:39:20 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,3 +4,5 @@ dependencies = ["torch", "torchvision"]
 # classification
 from models.logistic_regression import logistic_regression_model
 from models.tissue_tile_net import tissue_tile_net_model, tissue_tile_net_transform, tissue_tile_net_model_5_class
+
+from transforms.TileTissueNet5Class import TissueTileNetTransformer
